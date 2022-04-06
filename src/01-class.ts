cb376d5c9dbc3e29778914e073a0b9fc4c00d9dd
giancode1 <gieg5c@gmail.com>
2022-04-06 12:25:01 -0500

    Class

---
+++ src/01-class.ts
@@ -0,0 +1,29 @@
+const date = new Date();
+date.getHours();
+date.getTime();
+date.toISOString();
+
+const date2 = new Date(1995, 10, 12);
+date2.getHours();
+date2.getTime();
+date2.toISOString();
+
+console.log(date);
+console.log(date2);
+
+let myVar;
+
+class MyDate {
+  year: number;
+  month: number;
+  day: number;
+
+  constructor(year: number, month: number, day: number) {
+    this.year = year;
+    this.month = month;
+    this.day = day;
+  }
+}
+
+const myDate = new MyDate(2022, 2, 10);
+console.log(myDate);
